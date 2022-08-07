4f4be602b1e01ee18947b05d0b5626f522a4d09b
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-08-07 21:09:21 +0900

    ADD: emailにindex追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates :name, presence: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 end
