366d28e2db5ff48708b2aee15d4ec79d1280678a
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-08-08 17:22:22 +0900

    ADD: 新規登録画面にフォーム追加

--- app/javascript/components/templates/Registration.jsx
+++ app/javascript/components/templates/Registration.jsx
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Registration = () => {
-  return <div>新規登録</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirmation, setPasswordConfirmation] = useState('');
+  const handleSubmit = (event) => {
+    console.log('イベント発火');
+    event.preventDefault();
+  };
+  return (
+    <div>
+      <p>新規登録</p>
+
+      {/* onSubmit、onChangeイベントを追加 */}
+      <form onSubmit={handleSubmit}>
+        <input
+          type='email'
+          name='email'
+          placeholder='メールアドレス'
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <input
+          type='password'
+          name='password'
+          placeholder='パスワード'
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <input
+          type='password'
+          name='password_confirmation'
+          placeholder='確認用パスワード'
+          value={passwordConfirmation}
+          onChange={(event) => setPasswordConfirmation(event.target.value)}
+        />
+
+        <button type='submit'>登録</button>
+      </form>
+    </div>
+  );
 };
 
 export default Registration;
