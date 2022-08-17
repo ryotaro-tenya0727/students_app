ea1320bd15a625b0d396a33cc76e0c3b37306973
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-08-17 21:25:24 +0900

    ADD: GithubAPIのトークンを取得

--- app/controllers/api/v1/github_oauth_controller.rb
+++ app/controllers/api/v1/github_oauth_controller.rb
@@ -1,5 +1,13 @@
+Faraday.default_adapter = :net_http
+
 class Api::V1::GithubOauthController < ApplicationController
   def callback
-    byebug
+    conn = Faraday.new do |builder|
+      builder.request :json
+    end
+
+    response = conn.post(ENV['GITHUB_FETCH_TOKEN_URL'],{client_id: ENV['REACT_APP_CLIENT_ID'], client_secret: ENV['CLIENT_SECRET'], code: params[:code] })
+    # byebug
+    # redirect_to '/registration'
   end
 end
